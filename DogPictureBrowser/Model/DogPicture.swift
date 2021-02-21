c280721105348bf4d7cfbfcf69823b716eb2edb3
CharleySun <Ysun@lcs.on.ca>
2021-02-21 15:34:43 -0500

    Added the search bar view.

---
+++ DogPictureBrowser/Model/DogPicture.swift
@@ -0,0 +1,8 @@
+//
+//  DogPicture.swift
+//  DogPictureBrowser
+//
+//  Created by Charley Sun on 2021-02-21.
+//
+
+import Foundation
---
+++ DogPictureBrowser/Model/SearchResult.swift
@@ -0,0 +1,8 @@
+//
+//  SearchResult.swift
+//  DogPictureBrowser
+//
+//  Created by Charley Sun on 2021-02-21.
+//
+
+import Foundation
