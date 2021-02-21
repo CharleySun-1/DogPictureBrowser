a56ce22f56cdacd1ca50d89e4d8e987b1e8a7a60
CharleySun <Ysun@lcs.on.ca>
2021-02-21 15:23:33 -0500

    Initial Commit

---
+++ DogPictureBrowser/DogPictureBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogPictureBrowserApp.swift
+//  DogPictureBrowser
+//
+//  Created by Charley Sun on 2021-02-21.
+//
+
+import SwiftUI
+
+@main
+struct DogPictureBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
