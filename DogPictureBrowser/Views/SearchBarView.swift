75ba8829d299ca99ebadc99ebf568dbeec81cad9
CharleySun <Ysun@lcs.on.ca>
2021-02-21 17:07:34 -0500

    Changed the URL.

--- DogPictureBrowser/Views/SearchBarView.swift
+++ DogPictureBrowser/Views/SearchBarView.swift
@@ -1,80 +1,94 @@
-    func fetchJoke() {
-        
-        // Set the address of the JSON endpoint
-        let url = URL(string: "https://icanhazdadjoke.com")!
+//
+//  SearchBarView.swift
+//  SwiftUI-Chicago-HSE-LCS
+//
+//  Created by Russell Gordon on 2021-02-14.
+//  Copyright © 2021 Gordon, Russell. All rights reserved.
+//
 
-        // Configure a URLRequest instance
-        // Defines what type of request will be sent to the address noted above
-        var request = URLRequest(url: url)
-        request.setValue("application/json",
-                         forHTTPHeaderField: "Accept")
-        request.httpMethod = "GET"
+import SwiftUI
 
-        // Run the request on a background thread and process the result.
-        // NOTE: This occurs asynchronously.
-        //       That means we don't know precisely when the request will
-        //       complete.
-        URLSession.shared.dataTask(with: request) { data, response, error in
-
-            // When the request *does* complete, there are three parameters
-            // containing data that are created:
-            //
-            // data
-            // The data returned by the server.
-            //
-            // response
-            // An object that provides response metadata, such as HTTP headers and status code. If you are making an HTTP or HTTPS request, the returned object is actually an HTTPURLResponse object.
-            //
-            // error
-            // An error object that indicates why the request failed, or nil if the request was successful.
-
-
-            // Verify that some data was actually returned
-            guard let jokeData = data else {
-
-                // When no data is returned, provide a descriptive error
-                //
-                // error?.localizedDescription is an example of "optional chaining"
-                // This means that if the error object is not nil, the
-                // localizedDescription property will be used
-                //
-                // ?? "Unknown error" is an example of the "nil coalescing" operator
-                // This means that when the error object *is* nil, a default string of
-                // "Unknown error" will be provided
-                print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
-
-                // Don't continue past this point
-                return
-
-            }
-
-            // DEBUG: See what raw JSON data was returned from the server
-            //print(String(data: jokeData, encoding: .utf8)!)
-
-            // Attempt to decode the JSON into an instance of the DadJoke structure
-            if let decodedJokeData = try? JSONDecoder().decode(DadJoke.self, from: jokeData) {
-
-                // DEBUG:
-                print("Joke data decoded from JSON successfully")
-                print("The joke is: \(decodedJokeData.joke)")
-
-                // Now, update the UI on the main thread
-                DispatchQueue.main.async {
-
-                    // Assign the result to the "someText" stored property
-                    someText = decodedJokeData.joke
-
+struct SearchBarView: View {
+    
+    var placeholderText: String = "Search..."
+    
+    @Binding var text: String
+    
+    @State private var isEditing = false
+    
+    // Whether to animate changes to the search field appearance
+    @State private var animateSearchField = false
+    
+    // Initialize a timer that will fire in one second
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        HStack {
+            
+            TextField(placeholderText, text: $text)
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+                        
+                        if isEditing && !text.isEmpty {
+                            Button(action: {
+                                text = ""
+                            }) {
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundColor(.gray)
+                                    .padding(.trailing, 8)
+                            }
+                        }
+                    }
+                )
+                .padding(.horizontal, 10)
+                .onTapGesture {
+                    isEditing = true
                 }
-
-            } else {
-
-                print("Could not decode JSON into an instance of the DadJoke structure.")
-
+                // Make the search field animate as it changes shape
+                // (Except when it first appears in the view)
+                .animation(animateSearchField ? .default : .none)
+            
+            if isEditing {
+                Button(action: {
+                    
+                    // Stop editing and clear search text
+                    isEditing = false
+                    text = ""
+                    
+                    // Dismiss the keyboard
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                }) {
+                    Text("Cancel")
+                }
+                .padding(.trailing, 10)
+                .padding(.bottom, -2)
+                .transition(.move(edge: .trailing))
+                .animation(.default)
             }
-
-        }.resume()
-        // NOTE: Invoking the resume() function
-        // on the dataTask closure is key. The request will not
-        // run, otherwise.
+        }
+        .onReceive(timer) { input in
+            
+            // Set the flag to apply animation to the search field
+            animateSearchField = true
+            
+            // Stop the timer
+            timer.upstream.connect().cancel()
+            
+        }
 
     }
+}
+
+struct SearchBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchBarView(text: .constant(""))
+    }
+}
