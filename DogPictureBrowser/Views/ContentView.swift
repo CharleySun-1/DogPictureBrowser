b82357d17a7ada7926437e4fbe3414cacfce2b11
CharleySun <Ysun@lcs.on.ca>
2021-02-22 13:21:47 -0500

    Added the remote item view into the VStack.

--- DogPictureBrowser/Views/ContentView.swift
+++ DogPictureBrowser/Views/ContentView.swift
@@ -1,90 +1,59 @@
 //
 //  ContentView.swift
-//  SongBrowser
+//  QuotesApp
 //
-//  Created by Charley Sun on 2021-02-20.
+//  Created by Charley Sun on 2021-02-19.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     
-    // MARK: Stored properties
-    
-    // Keeps track of what the user searches for
-    @State private var searchText = ""
-  
-    // Keeps the list of songs retrieved from Apple Music
-    @State private var pictures: [Picture] = [] //empty array to start
+    //MARK: Stored properties
+    @State var dogPictureUrl = "Hello, world!"
     
     
-    // MARK: Computed properties
+    //MARK: Computed properties
     var body: some View {
-        VStack{
-            
-         
-            SearchBarView(text: $searchText)
-                .onChange(of: searchText) { _ in
-                    fetchPictureResults()
-                }
-           
-            // Show a prompt when no search text is given
-            if searchText.isEmpty {
-                
-                
-                Spacer()
-                
-                Text("Please enter a breed of a dog")
-                    .font(.title)
-                    .foregroundColor(.secondary)
-                
-                Spacer()
-                
-                
-            } else {
-            
-            //Search text was given, results obtained
-            // Show the list of results
-            // Keypath of \. trackId tells the List view what property to use
-            // to uniquely identify each song
-                List(pictures, id: \.message) { currentPicture in
-                
-                    VStack(alignment: .leading) {
-                        
-                        Text(currentPicture.message)
-                        
-                    
-                        Text(currentPicture.message)
-                            .font(.caption)
-                    }
-                }
-                
-            }
-    }
-}
-    
-    // MARK: Functions
-    func fetchPictureResults() {
         
-        // Sanitize the search input
-        let input = searchText.lowercased().replacingOccurrences(of: " ", with: "+")
+        
+        VStack {
+            Text(dogPictureUrl)
+                .padding()
+                .onAppear() {
+                    // Invoke the function to get a joke
+                    //This is the call site
+                    fetchPicture()
+                }
+            
+            RemoteImageView(imageUrl: dogPictureUrl)
+            
+            
+            Button("Get another picture") {
+                fetchPicture()
+            }
+        }
+    }
+    
+    //MARK: Functions
+    func fetchPicture() {
         
         // Set the address of the JSON endpoint
-        let url = URL(string: "https://dog.ceo/api/\(input)/list/all")!
-
+        let url = URL(string: "https://dog.ceo/api/breeds/image/random")!
+        
         // Configure a URLRequest instance
         // Defines what type of request will be sent to the address noted above
         var request = URLRequest(url: url)
         request.setValue("application/json",
                          forHTTPHeaderField: "Accept")
         request.httpMethod = "GET"
-
+        
         // Run the request on a background thread and process the result.
         // NOTE: This occurs asynchronously.
         //       That means we don't know precisely when the request will
         //       complete.
         URLSession.shared.dataTask(with: request) { data, response, error in
-
+            
             // When the request *does* complete, there are three parameters
             // containing data that are created:
             //
@@ -96,11 +65,11 @@ struct ContentView: View {
             //
             // error
             // An error object that indicates why the request failed, or nil if the request was successful.
-
-
+            
+            
             // Verify that some data was actually returned
             guard let pictureData = data else {
-
+                
                 // When no data is returned, provide a descriptive error
                 //
                 // error?.localizedDescription is an example of "optional chaining"
@@ -111,42 +80,43 @@ struct ContentView: View {
                 // This means that when the error object *is* nil, a default string of
                 // "Unknown error" will be provided
                 print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
-
+                
                 // Don't continue past this point
                 return
-
+                
             }
-
+            
             // DEBUG: See what raw JSON data was returned from the server
-            //print(String(data: jokeData, encoding: .utf8)!)
-
-            // Attempt to decode the JSON into an instance of the SearchResult structure
-            if let decodedPictureData = try? JSONDecoder().decode(SearchResult.self, from: pictureData) {
-
+            print(String(data: pictureData, encoding: .utf8)!)
+            
+            // Attempt to decode the JSON into an instance of the DadJoke structure
+            if let decodedPictureData = try? JSONDecoder().decode(Picture.self, from: pictureData) {
+                
                 // DEBUG:
-                print("Song data decoded from JSON successfully")
-
+                
+                print("The Picture is: \(decodedPictureData.message)")
+                
                 // Now, update the UI on the main thread
                 DispatchQueue.main.async {
-
-                    // Assign the result to the "songs" property
-                    pictures = decodedPictureData.results
-
+                    
+                    // Assign the result to the "dogPictureUrl" stored property
+                    dogPictureUrl = decodedPictureData.message
                 }
-
+                
             } else {
-
-                print("Could not decode JSON into an instance of the SearchResult structure.")
-
+                
+                print("Could not decode JSON into an instance of the Picture structure.")
+                
             }
-
+            
         }.resume()
         // NOTE: Invoking the resume() function
         // on the dataTask closure is key. The request will not
         // run, otherwise.
-
+        
     }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
